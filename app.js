e374f1c99297762d5596ed94be19ba0c5efde492
Mahmoud Abbas <mahmoudabbas393@gmail.com>
2026-05-31 15:47:08 +0300

    feat: add data sanitization middleware for NoSQL injection and XSS protection

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
+
 const toursRouter = require('./routes/toursRoutes');
 const usersRouter = require('./routes/usersRoutes');
 const AppError = require('./utils/appError');
@@ -35,6 +39,26 @@ app.use(helmet());
 app.use(
   express.json({
     limit: '10kb',
+  }),
+);
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
+
+// Prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsAverage',
+      'ratingsQuantity',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ],
   }),
 );
 
