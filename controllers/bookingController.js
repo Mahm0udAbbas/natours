e33b65edd76901df7af42ecc0c1ab8c209e43b85
Mahmoud Abbas <mahmoudabbas393@gmail.com>
2026-07-11 19:36:59 +0300

    feat: implement booking model and integrate checkout session handling

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -2,8 +2,10 @@ const Stripe = require('stripe');
 
 const Tour = require('../models/tourModel');
 const catchAsync = require('../utils/catchAsync');
+const Booking = require('../models/bookingModel');
 
 const stripeClient = Stripe(process.env.STRIPE_SECRET_KEY);
+
 exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   // 1) Get the currently booked tour
   const tour = await Tour.findById(req.params.tourId);
@@ -12,7 +14,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   const session = await stripeClient.checkout.sessions.create({
     payment_method_types: ['card'],
     mode: 'payment',
-    success_url: `${req.protocol}://${req.get('host')}/`,
+    success_url: `${req.protocol}://${req.get('host')}/?tour=${req.params.tourId}&user=${req.user.id}&price=${tour.price}`,
     cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
     customer_email: req.user.email,
     client_reference_id: req.params.tourId,
@@ -39,4 +41,16 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
     status: 'success',
     session,
   });
+});
+
+exports.createBookingCheckout = catchAsync(async (req, res, next) => {
+  const { tour, user, price } = req.query;
+  console.log(tour, user, price);
+
+  if (!tour && !user && !price) {
+    return next();
+  }
+
+  await Booking.create({ tour, user, price });
+  res.redirect(req.originalUrl.split('?')[0]);
 });
--- routes/viewsRoutes.js
+++ routes/viewsRoutes.js
@@ -9,9 +9,10 @@ const {
   getAccount,
 } = require('../controllers/viewsController');
 const { isLoggedin, protect } = require('../controllers/authController');
+const { createBookingCheckout } = require('../controllers/bookingController');
 
 const router = express.Router();
-router.get('/', isLoggedin, getOverview);
+router.get('/', createBookingCheckout, isLoggedin, getOverview);
 router.get('/overview', isLoggedin, getOverview);
 router.get('/tour/:slug', isLoggedin, getTour);
 router.get('/login', isLoggedin, getLoginForm);
